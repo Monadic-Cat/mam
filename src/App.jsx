f8a39d3ca960e56f03ee8e6f8948714758c02f08
Monadic Cat <greenmonadcat@gmail.com>
2019-01-07 13:07:40 -0600

    Indent with tabs, app outline.

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Labels from './Labels';
+import Conditions from './Conditions';
+import Potential from './Potential';
+import Harm from './Harm';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<Labels />
+				<Conditions />
+				<Potential />
+				<Harm />
+			</div>
+		);
+	}
 }
 
 export default App;
