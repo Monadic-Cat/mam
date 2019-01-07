82e8d935aabfd6f31a22d5a944c12eb0196a4356
Monadic Cat <greenmonadcat@gmail.com>
2019-01-07 13:54:19 -0600

    Partially implement Conditions component

--- src/Conditions.jsx
+++ src/Conditions.jsx
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
+import './Conditions.css';
 
 class Condition extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			marked: false
+		};
+	}
+	handleClick = () => {
+		this.setState((state, props) => ({
+			marked: !state.marked
+		}));
+	}
 	render() {
 		return (
-			<div className="Condition">
+			<td
+				className={"Condition" + (this.state.marked ? " marked-condition":"")}
+				onClick={() => this.handleClick()}
+			>
 				{this.props.name}
-			</div>
+			</td>
 		);
 	}
 }
@@ -13,12 +28,14 @@ class Condition extends Component {
 class Conditions extends Component {
 	render() {
 		return (
-			<div className="Conditions">
+			<div>
+			<table className="Conditions"><tbody><tr>
 				<Condition name="Afraid" />
 				<Condition name="Angry" />
 				<Condition name="Guilty" />
 				<Condition name="Hopeless" />
 				<Condition name="Insecure" />
+			</tr></tbody></table>
 			</div>
 		);
 	}
