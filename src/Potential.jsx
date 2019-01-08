506a5394ec78db23ce8203fc3efe0aee938bfdfe
Monadic Cat <greenmonadcat@gmail.com>
2019-01-07 22:33:09 -0600

    Partially implement Potential component

--- src/Potential.jsx
+++ src/Potential.jsx
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import { range } from 'lodash';
+
+function group(n, groupSize) {
+	let groups = range(0, n / groupSize | 0).map(x => groupSize);
+	groups.push(n % groupSize);
+	return groups;
+}
 
 class Potential extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			amount: 15
+		};
+	}
+
 	render() {
+		var a = 0;
 		return (
 			<div className="Potential">
-				5
+			<table><tbody><tr>
+			{group(this.state.amount, 5).map(x => <td key={a++}>{x}</td>)}
+			</tr></tbody></table>
 			</div>
 		);
 	}
