732f28b14aaf177fcc1c4c038d61a293a4a37a54
Monadic Cat <greenmonadcat@gmail.com>
2019-02-07 15:56:38 -0600

    Fix indentation

--- src/types.js
+++ src/types.js
@@ -1,36 +1,36 @@
 //@flow
 
 export type OrderedDict<k, v> = {
-  elements: Array<v>,
-  order: {
-    [k]: number
-  }
+	elements: Array<v>,
+	order: {
+		[k]: number
+	}
 }
 
 export type LabelState = {
-  name: string,
-  value: number
+	name: string,
+	value: number
 }
 
 export type ConditionState = {
-  name: string,
-  marked: boolean
+	name: string,
+	marked: boolean
 }
 
 export type PotentialState = {
-  total: number,
-  used: number,
-  groupSize: number
+	total: number,
+	used: number,
+	groupSize: number
 }
 
 export type CharacterState = {
-  player: string,
-  name: string,
-  heroName: string,
-  playbook: string,
-  powers: string,
-  labels: OrderedDict<string, LabelState>,
-  conditions: OrderedDict<string, ConditionState>,
-  potential: PotentialState,
-  harm: number
+	player: string,
+	name: string,
+	heroName: string,
+	playbook: string,
+	powers: string,
+	labels: OrderedDict<string, LabelState>,
+	conditions: OrderedDict<string, ConditionState>,
+	potential: PotentialState,
+	harm: number
 }
